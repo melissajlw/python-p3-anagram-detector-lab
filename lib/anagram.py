18b091e63f80293c9f892d9d9eba0d2ef695358c
Melissa Wang <melissa.wang21@gmail.com>
2024-10-07 17:49:11 -0400

    anagram detector lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        result = [w for w in words if sorted(w) == sorted(self.word)]
+        return result
